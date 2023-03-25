7a72c5d54613692a87857df8f0ea68babb74590e
owais <owaes.hamouda.2000@gmail.com>
2023-03-25 16:52:41 +0200

    Names Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math' as math;
+
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,6 +22,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const names = [
+  "anas",
+  "owais",
+  "ahmad",
+];
+
+extension RandomIterable<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void getRandomName() => emit(names.getRandomElement());
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -27,13 +46,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late final NamesCubit namesCubit;
+
+  @override
+  void initState() {
+    namesCubit = NamesCubit();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    namesCubit.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Bloc Course"),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          children: [
+            StreamBuilder(
+              stream: namesCubit.stream,
+              builder: (_, snabshot) {
+                switch (snabshot.connectionState) {
+                  case ConnectionState.none:
+                    return const SizedBox.shrink();
+                  case ConnectionState.waiting:
+                    return const SizedBox.shrink();
+                  case ConnectionState.active:
+                    return Text(snabshot.data ?? '');
+                  case ConnectionState.done:
+                    return const SizedBox.shrink();
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () => namesCubit.getRandomName(),
+              child: const Text("Pick Random Names"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
